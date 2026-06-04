89ec5f4bed5322e2899cbe9d3c9dc3dc9f9e3673
md-asik-reza-tasin <mdasikreza1997@gmail.com>
2026-06-04 12:33:12 +0600

    ROLE BASED MIDDLEWARE

---
+++ src/middleware/role.middleware.js
@@ -0,0 +1,19 @@
+﻿exports.authorize = (...roles) => {
+  return (req, res, next) => {
+    if (!req.user) {
+      return res.status(401).json({
+        success: false,
+        message: "You do not have permission to access this route",
+      });
+    }
+
+    if (!roles.includes(req.user.role)) {
+      return res.status(403).json({
+        success: false,
+        message: "You do not have permission to access this route",
+      });
+    }
+
+    next();
+  };
+};
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,11 +1,18 @@
 ﻿const express = require("express");
 const { register, login, getMe } = require("../controllers/auth.controller");
 const { protect } = require("../middleware/auth.middleware");
+const { authorize } = require("../middleware/role.middleware");
 
 const router = express.Router();
 
 router.post("/register", register);
 router.post("/login", login);
 router.get("/me", protect, getMe);
+router.get("/admin-test", protect, authorize("admin"), (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "Admin route access granted",
+  });
+});
 
 module.exports = router;
